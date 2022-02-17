ced85cc44c08c1082b5501c64a5859ec1a6764b2
brontasaurus <58154713+brontasaurus@users.noreply.github.com>
2022-02-16 20:00:04 -0800

    done with pixel art

--- src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,13 +1,14 @@
 package _04_Crazy_Digital_Painting;
-
+import java.awt.Color;
 public class CrazyDigitalPainting {
     // 1. Create two final static integers for the width and height of the display.
-
+final static int HEIGHT = 1200;
+final static int WIDTH = 1200;
 
     // 2. Create a 2D array of Color objects. You will need to import
     //    java.awt.Color. Initialize the size of the array using the 
     //    integers created in step 1.
-
+Color [][] colors = new Color[WIDTH][HEIGHT];
 
 
     public CrazyDigitalPainting() {
@@ -17,11 +18,16 @@ public class CrazyDigitalPainting {
         //    to a new color. The sample image was created using the following 
         //    pattern:
         //    colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-
+for (int col = 0; col < colors.length; col++) {
+	for (int row = 0; row < colors[col].length; row++) {
+		colors[col][row] = new Color((col * 137)%256, ((col + row) * 137)%256, (row * 137)%256);
+	}
+}
         // 5. Come up with your own pattern to make a cool crazy image.
 
         // 6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
         //    to show off your picture.
+ColorArrayDisplayer.displayColorsAsImage(colors);
     }
 
     public static void main(String[] args) {
--- src/_05_Pixel_Art/GridPanel.java
+++ src/_05_Pixel_Art/GridPanel.java
@@ -17,7 +17,7 @@ public class GridPanel extends JPanel{
     private int cols;
 
     // 1. Create a 2D array of pixels. Do not initialize it yet.
-
+Pixel [][] pixels;
     private Color color;
 
     public GridPanel(int w, int h, int r, int c) {
@@ -34,10 +34,14 @@ public class GridPanel extends JPanel{
         setPreferredSize(new Dimension(windowWidth, windowHeight));
 
         // 2. Initialize the pixel array using the rows and cols variables.
-
+        pixels = new Pixel[cols][rows];
 
         // 3. Iterate through the array and initialize each element to a new pixel.
-
+        for (int col = 0; col < pixels.length; col++) {
+        	for (int row = 0; row < pixels[col].length; row++) {
+        		pixels[col][row] = new Pixel(col, row);
+        	}
+        }
 
     }
 
@@ -48,6 +52,8 @@ public class GridPanel extends JPanel{
     public void clickPixel(int mouseX, int mouseY) {
         // 5. Use the mouseX and mouseY variables to change the color
         //    of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+    	
+    	pixels[mouseX/pixelWidth][mouseY/pixelHeight].color = color;
     }
 
     @Override
@@ -55,6 +61,18 @@ public class GridPanel extends JPanel{
         // 4. Iterate through the array.
         //    For every pixel in the list, fill in a rectangle using the pixel's color.
         //    Then, use drawRect to add a grid pattern to your display.
+    	for (int col = 0; col < pixels.length; col++) {
+        	for (int row = 0; row < pixels[col].length; row++) {
+        		g.setColor(pixels[col][row].color);
+        		g.fillRect(pixels[col][row].x*pixelWidth, pixels[col][row].y*pixelHeight, pixelWidth, pixelHeight);
+        	}
+        }
+    	g.setColor(Color.WHITE);
+    	for (int col = 0; col < pixels.length; col++) {
+        	for (int row = 0; row < pixels[col].length; row++) {
+        		g.drawRect(pixels[col][row].x*pixelWidth, pixels[col][row].y*pixelHeight, pixelWidth, pixelHeight);
+        	}
+    	}
 
     }
 }
